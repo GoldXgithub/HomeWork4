5617da188e830bb8bb91f32c53d08c5ddf17427a
Lev <levzol@ya.ru>
2026-05-19 15:36:41 +1000

    + B task 2

--- Task4_B.py
+++ Task4_B.py
@@ -15,11 +15,18 @@ class Money:
         if self.cop <= other.cop:
             result.cop *= -1
         return result
+    def __mul__(self, value):
+        result =  Money(self.rub * value, self.cop * value)
+        while result.cop >= 100:
+            result.cop -= 100
+            result.rub += 1
+        return result
 
 
-money_sum1 = Money(10, 30)
+money_sum1 = Money(15, 50)
 money_sum2 = Money(20, 60)
 print(money_sum1)
 money_result = money_sum1 - money_sum2
 print(money_result)
+print(2 * money_sum1)
         
